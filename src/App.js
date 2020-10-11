4aa107a87edf5df6975884b761c29dc05add71a4
Brandon Humphry <brandonhumphry@gmail.com>
2020-10-10 20:02:47 -0700

    moved LogIn component to its own file

--- src/App.js
+++ src/App.js
@@ -1,43 +1,13 @@
-import { render } from '@testing-library/react';
 import React from 'react';
 import './App.css';
+import LogIn from './components/LogIn'
 
-// function App() {
-//   return (
-//     <div>
-        // <Authentication>
-//     </div>
-//   );
-// }
-
-class App extends React.Component {
-  constructor(){
-    super()
-    this.state ={
-      isLoggedIn: false
-    }
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick() {
-    console.log("working")
-    this.setState(prevState => {
-      return {
-        isLoggedIn: !prevState.isLoggedIn
-      }
-    })
-  }
-
-render() {
-  let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN"
-  let displayText = this.state.isLoggedIn ? "Logged in" : "Logged out"
+function App() {
   return (
     <div>
-      <button onClick={this.handleClick}>{buttonText}</button>
-  <h1>{displayText}</h1>
+      <LogIn></LogIn>
     </div>
   )
 }
-}
 
-export default App;
+export default App
---
+++ src/components/LogIn.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import '../App.css';
+
+class LogIn extends React.Component {
+    constructor(){
+      super()
+      this.state ={
+        isLoggedIn: false
+      }
+      this.handleClick = this.handleClick.bind(this)
+    }
+  
+    handleClick() {
+      console.log("working")
+      this.setState(prevState => {
+        return {
+          isLoggedIn: !prevState.isLoggedIn
+        }
+      })
+    }
+  
+  render() {
+    let buttonText = this.state.isLoggedIn ? "LOG OUT" : "LOG IN"
+    let displayText = this.state.isLoggedIn ? "Logged in" : "Logged out"
+    return (
+      <div>
+        <button onClick={this.handleClick}>{buttonText}</button>
+    <h1>{displayText}</h1>
+      </div>
+    )
+  }
+  }
+
+export default LogIn
