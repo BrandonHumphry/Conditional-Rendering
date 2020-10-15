bcbac2f7562467e65ba452391fde522748161db9
Brandon Humphry <brandonhumphry@gmail.com>
2020-10-14 22:13:55 -0700

    added dropdown and checkbox options

--- src/components/Form.js
+++ src/components/Form.js
@@ -9,13 +9,19 @@ class Form extends Component {
             age: "",
             gender: "",
             destination: "",
-            dietaryRestrictions: []
+            isVegan: false,
+            isKosher: false,
         }
         this.handleChange = this.handleChange.bind(this)
     }
 
     handleChange(event){
-        const {name, value} = event.target
+        const {name, value, type, checked} = event.target
+        type === "checkbox" ?
+            this.setState({
+                    [name]:checked       
+            })
+            :
         this.setState({
             [name]: value
         })
@@ -46,11 +52,60 @@ class Form extends Component {
                     placeholder="Age" />
                     <br/>
                     
-                    {/* <label>
-                        <input type="radio"/> Male
-                    </label> */}
-         
+                    <label>
+                        <input 
+                        type="radio"
+                        name="gender"
+                        value="male"
+                        checked={this.state.gender === "male"}
+                        onChange={this.handleChange}
+                        />Male
+                    </label>
+
+                    <label>
+                        <input 
+                        type="radio"
+                        name="gender"
+                        value="female"
+                        checked={this.state.gender === "female"}
+                        onChange={this.handleChange}
+                        />Female
+                    </label>
                     <br/>
+
+                    <select 
+                        value={this.state.destination} 
+                        name="destination" 
+                        onChange={this.handleChange}
+                    >
+                        <option value="--">Please select a destination</option>
+                        <option value="Germany">Germany</option>
+                        <option value="Norway">Norway</option>
+                        <option value="France">France</option>
+                        <option value="Greece">Greece</option>
+                    </select>
+                    <br/>
+
+                    <label>
+                        <input
+                        type="checkbox"
+                        name="isVegan"
+                        onChange={this.handleChange}
+                        checked={this.state.isVegan}
+                        />Vegan
+                    </label>
+                    <br/>
+          
+                    <label>
+                        <input
+                        type="checkbox"
+                        name="isKosher"
+                        onChange={this.handleChange}
+                        checked={this.state.isKosher}
+                        />Kosher
+                    </label>
+                    <br/>
+                    
                     <button>Submit</button>
                     <br/>
                 </form>
@@ -58,11 +113,17 @@ class Form extends Component {
                 <h2>Entered Info</h2>
                 <p>Your name: {this.state.firstName} {this.state.lastName}</p>
                 <p>Your age: {this.state.age}</p>
-                {/* <p>Your gender: {this.state.gender}</p>
+                <p>Your gender: {this.state.gender}</p>
                 <p>Your destination: {this.state.destination}</p>
                 <p>
-                    Your dietary restrictions: {this.state.dietaryRestrictions}
-                </p> */}
+                    Your dietary restrictions: 
+                </p>
+                <p>
+                    Vegan: {this.state.isVegan ? "Yes" : "No"}
+                </p>
+                <p>
+                    Kosher: {this.state.isKosher ? "Yes" : "No"}
+                </p>
             </main>
         )
     }
